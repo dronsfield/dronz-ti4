2cb7e56b4d347ce0d4ee1e62bdb40b0bd71bbf06
Josh <josh.dronsfield@gmail.com>
2024-08-05 19:24:27 +0100

    add acs page

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true
+  },
+  redirects: () => ([
+    {
+      source: '/',
+      destination: '/ac',
+      permanent: true,
+    }
+  ])
 };
 
 export default nextConfig;
---
+++ src/scraping.ts
@@ -0,0 +1,18 @@
+"use client";
+
+export function removeLinks(htmlText: string): string {
+  // Regex to match <a> tags and capture their contents, including across multiple lines
+  const pattern = /<a\s+[^>]*>(.*?)<\/a>/gis;
+
+  // Replace <a> tags with their contents
+  let cleanedText = htmlText.replace(pattern, "$1");
+
+  // Remove all newline characters
+  cleanedText = cleanedText.replace(/\n/g, "");
+
+  return cleanedText;
+}
+
+if (typeof window !== "undefined") {
+  (window as any).rl = removeLinks;
+}
