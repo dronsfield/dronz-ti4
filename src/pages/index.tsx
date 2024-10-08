2703424799494597ae3cb17c8ddb2636bbbb8eba
Josh <josh.dronsfield@gmail.com>
2024-10-08 22:14:07 +0100

    style home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,10 +18,9 @@ const StyledContainer = styled(Container)`
 `;
 
 const LinksContainer = styled.ul`
-  margin-top: 1rem;
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
-  gap: 0.5rem; /* Space between boxes */
+  gap: 0.75em;
 `;
 
 const PageLink = styled(Link)`
@@ -34,19 +33,31 @@ const PageLink = styled(Link)`
   display: flex;
   flex-direction: row;
   align-items: center;
-  padding: 1em;
+  padding: 0.8em 0.8em;
   border: 1px solid #ddd;
   border-radius: 2px;
   text-transform: uppercase;
   font-weight: bold;
-  gap: 1em;
+  gap: 0.67em;
+  height: 2.5em;
+  border-radius: 2px;
 
-  img {
-    width: 1.3em;
-    object-fit: contain;
-    object-position: center center;
-    object-repeat: norepeat;
-    height: 1.5em;
+  &:focus,
+  &:active {
+    border-color: currentColor;
+  }
+
+  div {
+    width: 2.5em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-shrink: 0;
+    img {
+      height: 2.5em;
+
+      border-radius: 3px;
+    }
   }
 `;
 
@@ -54,67 +65,62 @@ const linksConfig = [
   {
     href: "/factions",
     text: "Factions",
-    image: "/hm",
-  },
-  {
-    href: "/ac",
-    text: "Action cards",
-    image: "/home/ac.jpeg",
+    image: "/home/empyrean.webp",
   },
   {
     href: "/tech",
     text: "Tech",
-    image: "/hm",
+    image: "/home/tech.png",
+  },
+  {
+    href: "/sc",
+    text: "Strategy cards",
+    image: "/home/imp.png",
+  },
+  {
+    href: "/ac",
+    text: "Action cards",
+    image: "/home/ac.jpg",
   },
   {
     href: "/po",
     text: "Public objectives",
-    image: "/hm",
+    image: "/home/po.jpg",
+  },
+  {
+    href: "/so",
+    text: "Secret objectives",
+    image: "/home/so.jpg",
+  },
+  {
+    href: "/exploration",
+    text: "Exploration cards",
+    image: "/home/cultural.webp",
+  },
+  {
+    href: "/relic",
+    text: "Relics",
+    image: "/home/relic.webp",
   },
 ];
 
 export function Home() {
   return (
     <StyledContainer>
-      <h1>TI4 Info</h1>
+      <h1 style={{ fontFamily: "monospace" }}>ti4.dronz</h1>
 
-      {/* <LinksContainer>
+      <LinksContainer>
         {linksConfig.map((config) => {
           return (
             <PageLink href={config.href}>
-              <img src={config.image} />
+              <div>
+                <img src={config.image} />
+              </div>
               <span>{config.text}</span>
             </PageLink>
           );
         })}
-      </LinksContainer> */}
-
-      <ul>
-        <li>
-          <Link href="/factions">Factions</Link>
-        </li>
-        <li>
-          <Link href="/tech">Tech</Link>
-        </li>
-        <li>
-          <Link href="/ac">Action cards</Link>
-        </li>
-        <li>
-          <Link href="/sc">Strategy cards</Link>
-        </li>
-        <li>
-          <Link href="/po">Public objectives</Link>
-        </li>
-        <li>
-          <Link href="/so">Secret objectives</Link>
-        </li>
-        <li>
-          <Link href="/exploration">Exploration cards</Link>
-        </li>
-        <li>
-          <Link href="/relic">Relics</Link>
-        </li>
-      </ul>
+      </LinksContainer>
     </StyledContainer>
   );
 }
